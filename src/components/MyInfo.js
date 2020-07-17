aaff0540dc0c0d21b3188a1ccd87bffd7b9669d0
vivekrao1985 <redeemned@gmail.com>
2020-07-17 13:22:01 -0400

    initial commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+	return (
+		<div>
+			<h1>Vivek Rao</h1>
+			<p>i am anti-you.</p>
+			<l>
+				<li>Martinique</li>
+				<li>Albaquerque</li>
+				<li>San Francisco</li>
+			</l>
+		</div>
+	)
+}
+
+export default MyInfo
